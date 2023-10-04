b0d7da82e69b406f71e968c31dc8f4965175e554
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-04 14:36:09 -0700

    Added all the necessary routes and pug pages

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+// const data = require('./data.json')
+// const projects= data.projects
+const mainRoutes = require('./routes');
+const projectRoutes = require('./routes/index.js');
+
+app.use(mainRoutes)
+app.use('/index', projectRoutes)
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+  });
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router= express.Router()
+const data = require('../data.json')
+const projects= data.projects
+
+router.get('/', (req, res)=>{
+    res.render('home')
+})
+
+router.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+router.get('/project/:id', (req, res)=>{
+    res.render('project')
+})
+
+
+
+
+
+
+
+module.exports= router
